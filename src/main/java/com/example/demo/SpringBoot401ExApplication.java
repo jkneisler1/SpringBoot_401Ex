98365ef7e09a60159afe2fd876031f8d981b36a4
GBTC405002UR <GBTC405002UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-21 11:19:13 -0400

    Exercise for project 4.01   Could not login to any page until I logged in with "user/password", then I could reach other sites.

---
+++ src/main/java/com/example/demo/SpringBoot401ExApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot401ExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot401ExApplication.class, args);
+    }
+
+}
